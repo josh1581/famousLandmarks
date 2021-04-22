33f04b44bac13cd17930e086397c14632fc6dd93
Joshua Hoyle <joshuahoyle@Joshuas-Air.hsd1.wa.comcast.net>
2021-04-22 16:01:20 -0700

    finished product

---
+++ famouseLandmark/famouseLandmark/Controllers/Model Controllers/LandmarkController.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkController.swift
+//  famouseLandmark
+//
+//  Created by Joshua Hoyle on 4/22/21.
+//
+
+import UIKit
+
+class LandmarkController {
+    
+    static var landmarks = [Landmark(landmarkName: "Statue of Liberty", landmarkLocation: "United States of America", landmarkImage: UIImage(named: "liberty")),
+                            Landmark(landmarkName: "St. Basil's Cathedral", landmarkLocation: "Russia", landmarkImage: UIImage(named: "cathedral")),
+                            Landmark(landmarkName: "Blue Domed Church", landmarkLocation: "Greece", landmarkImage: UIImage(named: "blueDomedChurch")),
+                            Landmark(landmarkName: "The Great Spinx", landmarkLocation: "Egypt", landmarkImage: UIImage(named: "sphinx")),
+                            Landmark(landmarkName: "The Little Mermaid", landmarkLocation: "Denmark", landmarkImage: UIImage(named: "littleMermaid")),
+                            Landmark(landmarkName: "Neptune and the Palace of Versailles", landmarkLocation: "France", landmarkImage: UIImage(named: "neptune")),
+                            Landmark(landmarkName: "Windmills", landmarkLocation: "Holland", landmarkImage: UIImage(named: "windmills")),
+                            Landmark(landmarkName: "The Great Wall of China", landmarkLocation: "China", landmarkImage: UIImage(named: "greatWall")),
+                            Landmark(landmarkName: "The Taj Mahal", landmarkLocation: "India", landmarkImage: UIImage(named: "tajMahal")),
+                            Landmark(landmarkName: "Machu Picchu", landmarkLocation: "Peru", landmarkImage: UIImage(named: "machuPicchu"))]
+}
